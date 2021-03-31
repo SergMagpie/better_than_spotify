a8f39a93a4fb6d799a2576ddf9e1c959cc6bde67
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-03-31 14:25:22 +0300

    up

--- project/func_music_library.py
+++ project/func_music_library.py
@@ -2,6 +2,8 @@ def add(elem_name: str) -> list:
     '''
     Provides the ability to add an item. 
     Returns the list of values of the element.
+    If the name of the element is "name of group" 
+    or "year of foundation", do not ask for the next element.
     '''
     pass
 
@@ -10,6 +12,8 @@ def change(elem_name: str, elem_value: str) -> list:
     '''
     Allows you to change the value of the item.
     Returns new list of values of element.
+    If the name of the element is "name of group" 
+    or "year of foundation", do not ask for the next element.
     '''
     pass
 
