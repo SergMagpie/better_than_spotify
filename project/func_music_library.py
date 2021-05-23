35497b3030a76638e4db7d716e5aa8dbfc75eff0
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-05-23 20:28:50 +0300

    up

--- project/func_music_library.py
+++ project/func_music_library.py
@@ -5,17 +5,29 @@ def add(elem_name: str) -> list:
     If the name of the element is "name of group" 
     or "year of foundation", do not ask for the next element.
     '''
-    pass
+    list_of_elem = []
+    next_elem = True
+    while next_elem:
+        list_of_elem.append(input('Enter {} '.format(elem_name)))
+        next_elem = (not elem_name in ("name of group", "year of foundation")
+                     and input('Add next {}? y/n '.format(elem_name)) == 'y')                    
+    return list_of_elem
 
 
-def change(elem_name: str, elem_value: str) -> list:
+def change(elem_name: str) -> list:
     '''
     Allows you to change the value of the item.
     Returns new list of values of element.
     If the name of the element is "name of group" 
     or "year of foundation", do not ask for the next element.
     '''
-    pass
+    list_of_elem = []
+    next_elem = True
+    while next_elem:
+        list_of_elem.append(input('Change {} '.format(elem_name)))
+        next_elem = (not elem_name in ("name of group", "year of foundation")
+                     and input('Add next {}? y/n '.format(elem_name)) == 'y')                    
+    return list_of_elem
 
 
 def delete(elem_name: str) -> bool:
@@ -24,4 +36,4 @@ def delete(elem_name: str) -> bool:
     If the answer YES returns the True 
     or a False if the answer is NO.
     '''
-    pass
+    return input('Delete {}? y/n '.format(elem_name)) == 'y'
